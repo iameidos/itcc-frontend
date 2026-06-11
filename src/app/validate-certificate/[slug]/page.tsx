52bc6ac1166f25c667fd394cd13b3cea17b70a51
iameidos <faizaldou@gmail.com>
2026-06-11 13:46:39 +0700

    Updated Colors

--- src/app/validate-certificate/[slug]/page.tsx
+++ src/app/validate-certificate/[slug]/page.tsx
@@ -36,7 +36,7 @@ export default async function ValidateCertificateDetailsPage(props: { params: Pr
             </div>
 
             <div className="flex-row items-center justify-items-center border p-4 lg:p-8 rounded-sm bg-white">
-              <div className="w-full text-center font-bold text-xl lg:text-2xl py-1.5 text-white bg-cert rounded-md leading-relaxed uppercase">{info.clientName}</div>
+              <div className="w-full text-center font-bold text-xl lg:text-2xl py-1.5 text-white bg-surv1 rounded-md leading-relaxed uppercase">{info.clientName}</div>
               <div className="grid md:grid-cols-3 gap-4 w-full justify-items-center py-8 px-2">
                 <div className="text-center text-sm space-y-2.5">
                   <h1>Scope of Certification :</h1>
@@ -57,18 +57,18 @@ export default async function ValidateCertificateDetailsPage(props: { params: Pr
               </div>
             </div>
 
-            {/* Ditampilkan hanya saat Telah Melaksanakan Surveillance 1st */}
+            {/* Ditampilkan hanya saat Telah Melaksanakan Sertifikasi 1st */}
             <div className="flex-row items-center justify-items-center border p-4 lg:p-8 rounded-sm bg-white my-8">
               <div className="w-full">
-                <div className="text-center py-2.5 text-white font-bold text-sm lg:text-base bg-cert rounded-md mb-4 leading-relaxed uppercase">First Surveillance</div>
+                <div className="text-center py-2.5 text-white font-bold text-sm lg:text-base bg-cert rounded-md mb-4 leading-relaxed uppercase">First Certification</div>
                 <div className="grid md:grid-cols-2 justify-center mx-auto items-center">
-                  <div className="text-sm h-[48px] bg-cert items-center text-white border flex pl-4">
+                  <div className="text-sm h-[48px] bg-surv1 items-center text-white border flex pl-4">
                     <p className="text-sm">Initial Date</p>
                   </div>
                   <div className="items-center h-[48px] border flex pl-4">
                     <p className="text-sm font-bold">{info.initialCertDate}</p>
                   </div>
-                  <div className="text-sm h-[48px] bg-cert items-center text-white border flex pl-4">
+                  <div className="text-sm h-[48px] bg-surv1 items-center text-white border flex pl-4">
                     <p className="text-sm">Expired Date</p>
                   </div>
                   <div className="items-center h-[48px] border flex pl-4">
@@ -78,18 +78,18 @@ export default async function ValidateCertificateDetailsPage(props: { params: Pr
               </div>
             </div>
 
-            {/* Ditampilkan hanya saat Telah Melaksanakan Surveillance 2nd */}
+            {/* Ditampilkan hanya saat Telah Melaksanakan Sertifikasi 2nd */}
             <div className="flex-row items-center justify-items-center border p-4 lg:p-8 rounded-sm bg-white my-8">
               <div className="w-full">
-                <div className="text-center py-2.5 text-white font-bold text-sm lg:text-base bg-cert rounded-md mb-4 leading-relaxed uppercase">Second Surveillance</div>
+                <div className="text-center py-2.5 text-white font-bold text-sm lg:text-base bg-cert rounded-md mb-4 leading-relaxed uppercase">Second Certification</div>
                 <div className="grid md:grid-cols-2 justify-center mx-auto items-center">
-                  <div className="text-sm h-[48px] bg-cert items-center text-white border flex pl-4">
+                  <div className="text-sm h-[48px] bg-surv1 items-center text-white border flex pl-4">
                     <p className="text-sm">Initial Date</p>
                   </div>
                   <div className="items-center h-[48px] border flex pl-4">
                     <p className="text-sm font-bold">{info.initialCertDate}</p>
                   </div>
-                  <div className="text-sm h-[48px] bg-cert items-center text-white border flex pl-4">
+                  <div className="text-sm h-[48px] bg-surv1 items-center text-white border flex pl-4">
                     <p className="text-sm">Expired Date</p>
                   </div>
                   <div className="items-center h-[48px] border flex pl-4">
@@ -101,15 +101,15 @@ export default async function ValidateCertificateDetailsPage(props: { params: Pr
 
             <div className="flex-row items-center justify-items-center border p-4 lg:p-8 rounded-sm bg-white">
               <div className="w-full">
-                <div className="text-center py-2.5 text-white font-bold text-sm lg:text-base bg-cert rounded-md mb-4 leading-relaxed uppercase">Certificate Status</div>
+                <div className="text-center py-2.5 text-white font-bold text-sm lg:text-base bg-surv1 rounded-md mb-4 leading-relaxed uppercase">Certificate Status</div>
                 <div className="grid md:grid-cols-4 justify-center gap-4 mx-auto border items-center">
-                  <div className="text-sm h-[48px] bg-cert items-center text-white flex pl-4">
+                  <div className="text-sm h-[48px] bg-surv1 items-center text-white flex pl-4">
                     <p className="text-sm">Certified Since</p>
                   </div>
                   <div className="">
                     <p className="text-sm font-bold">Friday, 07 February 2025</p>
                   </div>
-                  <div className="text-sm space-y-1.5 h-[48px] bg-cert items-center text-white flex pl-4">
+                  <div className="text-sm space-y-1.5 h-[48px] bg-surv1 items-center text-white flex pl-4">
                     <p className="text-sm">Certificate Status</p>
                   </div>
                   <div className="flex justify-center items-center text-sm">
@@ -118,13 +118,13 @@ export default async function ValidateCertificateDetailsPage(props: { params: Pr
                 </div>
 
                 <div className="grid md:grid-cols-4 gap-4 w-full border items-center">
-                  <div className="text-sm space-y-1.5 h-[48px] bg-cert items-center text-white flex pl-4">
+                  <div className="text-sm space-y-1.5 h-[48px] bg-surv1 items-center text-white flex pl-4">
                     <p className="text-sm">Valid From</p>
                   </div>
                   <div className="text-sm space-y-1.5">
                     <p className="text-sm font-bold">{info.initialCertDate}</p>
                   </div>
-                  <div className="text-sm space-y-1.5 h-[48px] bg-cert items-center text-white flex pl-4">
+                  <div className="text-sm space-y-1.5 h-[48px] bg-surv1 items-center text-white flex pl-4">
                     <p className="text-sm">Expired Date</p>
                   </div>
                   <div className="items-center flex">
@@ -134,15 +134,15 @@ export default async function ValidateCertificateDetailsPage(props: { params: Pr
               </div>
 
               <div className="w-full mt-8">
-                <div className=" text-center py-2.5 text-white font-bold text-sm lg:text-base bg-cert rounded-md mb-4 leading-relaxed uppercase">Surveillance Status</div>
+                <div className=" text-center py-2.5 text-white font-bold text-sm lg:text-base bg-surv1 rounded-md mb-4 leading-relaxed uppercase">Surveillance Status</div>
                 <div className="grid md:grid-cols-4 gap-4 w-full border items-center">
-                  <div className="text-sm space-y-1.5 h-[48px] bg-cert items-center text-white flex pl-4">
+                  <div className="text-sm space-y-1.5 h-[48px] bg-surv1 items-center text-white flex pl-4">
                     <p className="text-sm">First Surveillance</p>
                   </div>
                   <div className="text-sm space-y-1.5">
                     <p className="text-sm font-bold">{info.surv1Date}</p>
                   </div>
-                  <div className="text-sm space-y-1.5 h-[48px] bg-cert items-center text-white flex pl-4">
+                  <div className="text-sm space-y-1.5 h-[48px] bg-surv1 items-center text-white flex pl-4">
                     <p className="text-sm">First Surveillance Status</p>
                   </div>
                   <div className="flex justify-center items-center text-sm">
@@ -151,13 +151,13 @@ export default async function ValidateCertificateDetailsPage(props: { params: Pr
                 </div>
 
                 <div className="grid md:grid-cols-4 gap-4 w-full border items-center">
-                  <div className="text-sm space-y-1.5 h-[48px] bg-cert items-center text-white flex pl-4">
+                  <div className="text-sm space-y-1.5 h-[48px] bg-surv1 items-center text-white flex pl-4">
                     <p className="text-sm">Second Surveillance</p>
                   </div>
                   <div className="text-sm space-y-1.5">
                     <p className="text-sm font-bold">{info.surv2Date}</p>
                   </div>
-                  <div className="text-sm space-y-1.5 h-[48px] bg-cert items-center text-white flex pl-4">
+                  <div className="text-sm space-y-1.5 h-[48px] bg-surv1 items-center text-white flex pl-4">
                     <p className="text-sm">Second Surveillance Status</p>
                   </div>
                   <div className="flex justify-center items-center text-sm">
